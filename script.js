20c5b793800eb419c1a2b29ab8a48ff680692a4e
Swedish <tarthurf@yahoo.com>
2020-03-09 12:06:22 -0700

    Added prevent default on submit form, added text family and restyled accordingly

--- script.js
+++ script.js
@@ -1,38 +1,51 @@
+// Object with information to spawn in the popup element
 const information = {
     bio: "Though commonly called \"Tyler\", I am also know me as swedish, my online perona. I reside in San Diego, CA and am currently a full stack web development student due to graduate in June of 2020.",
     about: "This is my about me",
     skills: "I am fluent in HTML, CSS, SCSS, JavaScript (ES6), jQuery, multiple CSS frameworks, and Node.js.",
-    projects: "projects here",
+    projects: `<a href=""><span>Password Generator</span></a> / <a href=""><span>Day Planner</span></a> / <a href=""><span>Weather Dashboard</span></a> / <a href=""><span>MunchiMaps</span></a> / <a href=""><span>Team Builder</span></a> `,
     contact: 
     `<form action="https://formspree.io/xknravez" method="POST">
         <p>Message</p>
-        <input type="text" name="name">
+        <input type="text" name="name" placeholder="Let's get coffee!">
         <p>Email</p>
-        <input type="email" name="_replyto">
+        <input type="email" name="_replyto" placeholder="you@are-rad.com">
         <br>
-        <input id='submit' type="submit" value="Send">
+        <input id='submit' type="submit" value="Send" onClick='stopForm(event)'>
     </form>`
 }
 
-const modeToggle = $('.toggle')
-const iconsEl = $('.icons')
-const headerEl = $('.header')
-const buttonsEl = $('.buttons')
-const bioEl = $('#bio')
-const aboutEl = $('#about')
-const skillsEl = $('#skills')
-const projectsEl = $('#projects')
-const contactEl = $('#contact')
-const popUpEl = $('.pop-up')
 
+// Declaring jQuery variables
+const modeToggle = $('.toggle');
+const iconsEl = $('.icons');
+const headerEl = $('.header');
+const buttonsEl = $('.buttons');
+const bioEl = $('#bio');
+const aboutEl = $('#about');
+const skillsEl = $('#skills');
+const projectsEl = $('#projects');
+const contactEl = $('#contact');
+const popUpEl = $('.pop-up');
+const submitEl = $('#submit');
+
+
+
+// buttonsEl.hide()
 popUpEl.hide();
 
+const littleBright = () => {
+
+}
+
+// Darkmdoe toggle that switches class of bulb icon
 modeToggle.click(() => {
     if (modeToggle.children().attr('data-active') === 'true') modeToggle.children().attr('data-active', 'false')
     else if (modeToggle.children().attr('data-active') === 'false') modeToggle.children().attr('data-active', 'true')
     darkMode();
 })
 
+// Darkmode toggle css settings
 const darkMode = () => {
     if (modeToggle.children().attr('data-active') === 'true') {
         modeToggle.children().css('color', 'white')
@@ -51,12 +64,7 @@ const darkMode = () => {
 };
 
 
-// $('button').click(function() {
-    // buttonsEl.find(popUp).remove();
-    // popUp.text(information.bio);
-    // buttonsEl.append(popUp).fadeIn()
-// })
-
+// Function to transition one pop up to the next on button click
 $('button').click(function() {
     // popUpEl.text('')
     hideEl(popUpEl, 'fast');
@@ -66,11 +74,13 @@ $('button').click(function() {
         popUpShow();
     }, 500);
 });
-    
+
+// Generic hiding function
 const hideEl = (element, speed) => {
     element.hide(speed)
 }
 
+// Function brings up specific section on button click
 const popUpShow = () => {
     if (bioEl.attr('class') === 'active') popUpEl.html(information.bio);
     if (aboutEl.attr('class') === 'active') popUpEl.html(information.about);
@@ -78,4 +88,9 @@ const popUpShow = () => {
     if (projectsEl.attr('class') === 'active') popUpEl.html(information.projects);
     if (contactEl.attr('class') === 'active') popUpEl.html(information.contact);
     popUpEl.show('fast');
+}
+
+// Prevent page load on form submission
+const stopForm = (event) => {
+    event.preventDefault()
 }
