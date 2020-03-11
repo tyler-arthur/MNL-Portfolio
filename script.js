54934b2caa1b657426f3d7eeffb8cb4c548fa65a
Swedish <tarthurf@yahoo.com>
2020-03-11 00:29:27 -0700

    Added resume, added links to projects, updated bio and about me information.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Object with information to spawn in the popup element
 const information = {
-    bio: "Though commonly called \"Tyler\", I am also know me as swedish, my online perona. I reside in San Diego, CA and am currently a full stack web development student due to graduate in June of 2020.",
-    about: "This is my about me",
+    bio: `I believe in beauty through simplicity and always focus on a minimalistic approach to web development. Distraction-free design and intuitive navigation gives us more time to do the things we really want to be doing.<br><br> Websites should be responsive and fit our lifestyle, whether that be browsing on a mobile device while on the go or viewing a beautiful web page on a large display at home. Take a look at my <a href="./assets/images/Tyler_Arthur_Web_Dev_Resume.pdf">resume</a> and checkout some <a href="https://github.com/tarthurf">projects</a> I've made.`,
+    about: `I am passionate about the earth and its boundless beauty. I constantly think about the stars and how we came to ride spaceship Earth through the cosmos. I am an avid outdoorsman and rock climber. I am a life-long student. I love building web applications.<br><br>I was born and live in sunny, San Diego, California. I have worked in many fields ranging from retail and customer service, beer distribution, delivery driver, dog sitter and walker, building custom military grade rack servers, and teaching young children how to play Dungeons and Dragons.`,
     skills: "I am fluent in HTML, CSS, SCSS, JavaScript (ES6), jQuery, multiple CSS frameworks, and Node.js.",
-    projects: `<a href=""><i class="fas fa-key"></i><span> Password Generator</span></a> / <a href=""><i class="fas fa-calendar-check"></i><span> Day Planner</span></a> / <a href=""><i class="fas fa-cloud"></i><span> Weather Dashboard</span></a> / <a href=""><i class="fas fa-map-marked-alt"></i><span>MunchiMaps</span></a> / <a href=""><i class="fas fa-users-cog"></i><span> Team Builder</span></a> `,
+    projects: `<a href="https://github.com/tarthurf/Secure_Password_Generator"><i class="fas fa-key"></i><span> Password Generator</span></a> / <a href="https://github.com/tarthurf/Day-Planner"><i class="fas fa-calendar-check"></i><span> Day Planner</span></a> / <a href="https://github.com/tarthurf/Weather-Dashboard"><i class="fas fa-cloud"></i><span> Weather Dashboard</span></a> / <a href="https://github.com/tarthurf/MunchiMaps"><i class="fas fa-map-marked-alt"></i><span>MunchiMaps</span></a> / <a href="https://github.com/tarthurf/Employee-Generator"><i class="fas fa-users-cog"></i><span> Team Builder</span></a> `,
     contact: 
     `<form action="https://formspree.io/xknravez" method="POST">
         <p>Message</p>
@@ -94,3 +94,25 @@ const popUpShow = () => {
 const stopForm = (event) => {
     event.preventDefault()
 }
+
+// color randomizer
+function shuffle(array) {
+    var i = array.length,
+        j = 0,
+        temp;
+
+    while (i--) {
+
+        j = Math.floor(Math.random() * (i+1));
+
+        // swap randomly chosen element with current element
+        temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+
+    }
+
+    return array;
+}
+
+var ranNums = shuffle(["red", "orange", "green", "blue", "purple"]);
